eedcaa8db216df7604fdb2525b9eed1eef755a97
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-15 17:53:05 +0900

    Fix: Get~Properties now iterates through the superclass if available

--- KraftonEngine/Source/Engine/Object/UClass.cpp
+++ KraftonEngine/Source/Engine/Object/UClass.cpp
@@ -27,6 +27,8 @@ bool UClass::IsPropertyHidden(FString InName) const
 
 void UClass::GetEditableProperties(TArray<FProperty>& OutProps) const
 {
+	if (SuperClass) SuperClass->GetEditableProperties(OutProps);
+
 	for (uint32 i = 0; i < Properties.size(); i++)
 	{
 		if (!Properties[i]) continue;
@@ -37,6 +39,8 @@ void UClass::GetEditableProperties(TArray<FProperty>& OutProps) const
 
 void UClass::GetNonTransientProperties(TArray<FProperty>& OutProps) const
 {
+	if (SuperClass) SuperClass->GetNonTransientProperties(OutProps);
+
 	for (uint32 i = 0; i < Properties.size(); i++)
 	{
 		if (!Properties[i]) continue;
