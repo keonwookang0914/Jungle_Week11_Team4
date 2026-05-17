b1cc16a6d1b183326727512405cc83bb44543785
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-17 21:44:46 +0900

    SoftObjectPathRegistry header

---
+++ KraftonEngine/Source/Engine/Core/UObject/SoftObjectPathRegistry.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "FSoftObjectPath.h"
+#include "Core/Singleton.h"
+#include <unordered_map>
+
+class FSoftObjectPathRegistry : public TSingleton<FSoftObjectPathRegistry>
+{
+	friend class TSingleton<FSoftObjectPathRegistry>;
+
+public:
+
+
+private:
+	TMap<FSoftObjectPath, UObject*> SoftObjectPathRegistry;
+
+};
