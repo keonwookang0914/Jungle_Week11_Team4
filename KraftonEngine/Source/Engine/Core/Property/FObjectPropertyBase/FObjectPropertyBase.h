8a3cc77ab1703c66661fc38cafc544147a595094
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-17 17:36:31 +0900

    Update header for ObjectPropertyBase

--- KraftonEngine/Source/Engine/Core/Property/FObjectPropertyBase/FObjectPropertyBase.h
+++ KraftonEngine/Source/Engine/Core/Property/FObjectPropertyBase/FObjectPropertyBase.h
@@ -1,7 +1,19 @@
 ﻿#pragma once
 #include "Core/Property/PropertyTypes.h"
 
+class UClass;
+class UObject;
+
 class FObjectPropertyBase : public FProperty {
+public:
+	UClass* PropertyClass;
 
+	virtual EPropertyType GetType() const													= 0;
+	virtual json::JSON Serialize(const void* Instance) const								= 0;
+	virtual void Deserialize(void* Instance, const json::JSON& Value) const					= 0;
 
+	// Purpose:	Reads the raw `UObject*` stored at that memory address.
+	// Usage:	Fast and direct, but only works if the property is a "hard" object pointer.
+	virtual UObject* GetObjectPropertyValue(void* PropertyMemoryAddress) const				= 0;
+	virtual void SetObjectPropertyValue(void* PropertyMemoryAddress, UObject* Value) const	= 0;
 };
